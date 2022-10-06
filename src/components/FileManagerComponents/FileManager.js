a6ef2276a70cfd113c8e701657ed74b1e3b97524
Felipe Roman <feliperomanpirotti@gmail.com>
2022-10-06 18:23:36 -0300

    esqueleto del fileManager

---
+++ src/components/FileManagerComponents/FileManager.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import Filee from "./Filee";
+
+const FileManager = ({ files }) => {
+    return (
+        <ul className="arbol">
+            {files.map(file => <Filee name={file.name} type={file.type} files={file.files || []} />)}
+        </ul>
+    )
+}
+
+export default FileManager;
